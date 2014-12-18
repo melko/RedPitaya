72d8c0fb5ea1b1e8b120e213994975e4fc8bee93
Paolo Cretaro <paolocretaro@gmail.com>
2014-12-18 16:58:16 +0100

    listener.c:
    * increase pipe size to 256MB
    * change chunk size to 1MB

--- Test/dma/listener.c
+++ Test/dma/listener.c
@@ -1,5 +1,5 @@
 #define _GNU_SOURCE
-#define _FILE_OFFSET_BITS  64
+#define _FILE_OFFSET_BITS 64
 
 #include <stdio.h>
 #include <stdint.h>
@@ -7,7 +7,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-#define PACKET_SIZE 256
+#define PACKET_SIZE 1048576
 
 int main(int argc, char *argv[])
 {
@@ -15,13 +15,13 @@ int main(int argc, char *argv[])
 		printf("output file name is needed\n");
 		return -1;
 	}
-        int fd = open("fifo", O_RDONLY);
-        if(fd < 0){
-        	printf("error opening reading file\n");
-        	return -1;
-        }
-	printf("Setting pipe size: %d\n", fcntl(fd, F_SETPIPE_SZ, 1048576*10));
-        printf("Pipe size: %d\n", fcntl(fd, F_GETPIPE_SZ));
+	int fd = open("fifo", O_RDONLY);
+	if(fd < 0){
+		printf("error opening fifo\n");
+		return -1;
+	}
+	while(fcntl(fd, F_SETPIPE_SZ, 1048576*256) == -1);
+	printf("Pipe size: %d\n", fcntl(fd, F_GETPIPE_SZ));
 
 	int fd_out = creat(argv[1], S_IRUSR);
 	if(fd_out < 0){
@@ -30,9 +30,10 @@ int main(int argc, char *argv[])
 	}
 	printf("fadvise: %d\n", posix_fadvise(fd_out, 0, 0, POSIX_FADV_NOREUSE));
 
-	uint64_t buffer[PACKET_SIZE];
-	while( read(fd, buffer, PACKET_SIZE*8) ){
-		write(fd_out, buffer, PACKET_SIZE*8);
+	unsigned size;
+	uint64_t buffer[PACKET_SIZE/8];
+	while( size=read(fd, buffer, PACKET_SIZE) ){
+		write(fd_out, buffer, size);
 	}
 
 	//close(fd);
